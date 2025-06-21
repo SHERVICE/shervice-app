1374aa8b1faedaa22fbc13b9ba67c7ac5f0cf0e4
Vitor shermon <victorshermon@yahoo.com>
2025-06-21 19:40:07 -0300

    feat: add fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,11 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    PoppinsRegular: require('../assets/fonts/Poppins-Regular.ttf'),
+    PoppinsMedium: require('../assets/fonts/Poppins-Medium.ttf'),
+    PoppinsSemiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+    PoppinsBold: require('../assets/fonts/Poppins-Bold.ttf'),
+    PoppinsLight: require('../assets/fonts/Poppins-Light.ttf'),
   });
 
   if (!loaded) {
