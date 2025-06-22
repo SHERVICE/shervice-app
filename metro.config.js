6fd99f1a0d192a14576ddcc6c37f9a7bbfaf8672
Vitor shermon <victorshermon@yahoo.com>
2025-06-22 19:43:40 -0300

    feat: address view

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,17 @@ const {
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
 module.exports = wrapWithReanimatedMetroConfig(
   withNativeWind(config, { input: './global.css' }),
 );
