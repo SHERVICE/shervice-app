bf2298b5d2ee289b6a28a2efd6d9ee4ee5645e0c
Vitor Shermon <66441063+shermondevelopment@users.noreply.github.com>
2025-10-11 14:17:25 -0300

    feat: sentry monitory (#24)

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 const {
   wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 config.transformer = {
   ...config.transformer,
